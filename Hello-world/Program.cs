ff9a8bd4b17cedb60afe1156e302b78c7b525b0e
Matts Årling <matts.arling@arvika.xnse.com>
2016-03-23 10:08:47 +0100

    First commit

---
+++ Hello-world/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            Console.WriteLine("Hello world...");
+            Console.ReadKey();
+        }
+    }
+}
